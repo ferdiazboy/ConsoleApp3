af8f5b605a4b44beb503213cc35ebc8329579ec4
ferdiazboy <ferdiazboy@gmail.com>
2021-01-19 02:50:20 +0300

    refout ve methods eklendi

---
+++ RefOut/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kenar1 = 5;
+            int kenar2 = 4;
+
+            
+            Console.WriteLine(DiktortgenAlaniHesapla(kenar1,kenar2));// alan 8 olarak hesaplandı. 
+
+            //Console.WriteLine(DiktortgenAlaniHesapla(ref veya outkenar1, kenar2)); 
+            //Eğer kenar1 ref veya out olarak tanımlansaydı Kenar1 2 olarak hesaplanırdı
+
+            Console.WriteLine(kenar1); //kenar1 5
+        }
+
+        //static int DiktortgenAlaniHesapla(ref veya out int kenar1, int kenar2)
+        static int DiktortgenAlaniHesapla(int kenar1, int kenar2) 
+        {
+            kenar1 = 2;
+            return kenar1 * kenar2;
+        }
+    }
+}
