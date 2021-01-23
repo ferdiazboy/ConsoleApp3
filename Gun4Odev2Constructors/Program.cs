aa9d687782fee15a48ce9dea4f9ffd5de2cd4759
ferdiazboy <ferdiazboy@gmail.com>
2021-01-23 18:29:29 +0300

    4.gün ödevleri

---
+++ Gun4Odev2Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Gun4Odev2Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Ferdi", LastName = "Azboy", City = "Diyarbakır" };
+
+            Customer customer2 = new Customer(2,"Serhat","Azboy","Diyarbakır");
+
+            Console.WriteLine(customer2.FirstName);
+
+
+        }
+ 
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Gun4Odev3Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gun4Odev3Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            Console.WriteLine(sehirler.Count);
+
+           // sehirler.Add("Diyarbakır");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            Console.WriteLine(sehirler2.Count);
+            //sehirler2.Add("Diyarbakır");
+        }
+    }
+    class MyList<T> //Generic class
+    {
+        T[] _array;
+        public void Add(T item)
+        {
+            _array = new T[0];
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
